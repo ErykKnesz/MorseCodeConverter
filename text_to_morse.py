0ea1bcac679b41f34e6cc046822d933a32027c37
Eryk Knesz <eryk.knesz@gmail.com>
2022-05-21 12:00:26 +0200

    create morse code dict and converter function

---
+++ text_to_morse.py
@@ -0,0 +1,38 @@
+morse_code = {
+    'a': '· −',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+}
+
+
+def convert_to_morse(text):
+    morse_output = ""
+    for char in text:
+        for k in morse_code:
+            if char == k:
+                morse_output += morse_code[k]
+    return morse_output
+
